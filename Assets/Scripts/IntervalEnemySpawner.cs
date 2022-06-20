117e7b24aaf169fc431a4acbf69fdfedaa69e789
BradleyKernPF <bradley.kern@peoplefun.com>
2022-06-20 16:51:42 -0500

    Enemy Spawner Done

--- Assets/Scripts/IntervalEnemySpawner.cs
+++ Assets/Scripts/IntervalEnemySpawner.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class IntervalEnemySpawner : MonoBehaviour
 {
-    public float spawnInterval;
+    [SerializeField] private GameObject prefabSpawner;
+    [SerializeField] private GameObject[] enemyPool;
     
+    public float spawnInterval = 2f;
     
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 1.0f, spawnInterval);
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemyPool.Length < 1) return;
+        
+        GameObject go = Instantiate(prefabSpawner);
+        PrefabSpawner ps = go.GetComponent<PrefabSpawner>();
+        ps.prefabToSpawn = enemyPool[Random.Range(0, enemyPool.Length)];
+    }
 }
